8abaac311522e5a4258a9853da40fe96ce61bb84
Edgar F <eflores.ef@gmail.com>
2019-03-01 12:40:09 -0800

    Project II: Set up import/export and component skeleton to return in each file.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,10 +5,18 @@ const Footer = () => {
     return (
         <div className="footer-container">
             <div className="footer">
-                <i class="far fa-comment"></i>
-                <i class="fas fa-retweet"></i>6
-                <i class="far fa-heart"></i>4
-                <i class="far fa-envelope"></i>
+                <i className="far fa-comment" onClick={() => {
+                    document.querySelector('.fa-comment').style.color ="blue";
+                }}></i>
+                <i className="fas fa-retweet" onClick={() => {
+                    document.querySelector('.fa-retweet').style.color = "limegreen";
+                    document.querySelector('#retweetCounter').textContent = "7";
+                }}></i><div id="retweetCounter">6</div>
+                <i className="far fa-heart" onClick={() => {
+                    document.querySelector('.fa-heart').style.color = "red";
+                    document.querySelector('#likedCounter').textContent = "5";
+                }}></i><div id="likedCounter">4</div>
+                <i className="far fa-envelope"></i>
             </div>
         </div>
     )
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+    return (
+        <div>
+            Number Buttons
+        </div>
+    )
+}
+
+export default NumberButton;
